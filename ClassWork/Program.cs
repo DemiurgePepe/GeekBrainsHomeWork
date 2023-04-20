9036b25fd1922d5707248b3aa12c9adc687a499d
TheDemiurgePepe <orlovskiy-maksimka@mail.ru>
2023-04-20 23:13:59 +0500

    end classwork

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -4,4 +4,35 @@
 // Console.WriteLine($"quad = {(num*num)}");
 
 
-// // task2. 
+// // task2. Является ли первое число квадратом второго
+
+// Console.Write("input number A for check quad:");
+// int a=int.Parse(Console.ReadLine());
+// Console.Write("input number B for take a quad:");
+// int b=int.Parse(Console.ReadLine());
+// if(a==b*b)
+// {
+// Console.Write($"{a} quad {b}");
+// }
+// else
+// {
+//     Console.Write($"{a} not quad {b}");
+// }
+
+// Task3. Напишите программу которая принимает на вход число N а на выходе мы получаем строчку от -N до N
+// Console.Write("input number N:");
+// int N=int.Parse(Console.ReadLine());
+// if (N<=0) Console.WriteLine("not correct number");
+// int count=N*(-1);
+// while(count<=N)
+// {
+//     Console.Write(count+" ");
+//     count++;
+// }
+
+// task3.  на вход принимаем 3х значное число
+// вывести последнюю цифру числа
+Console.Write("input three-digit number N:");
+int N=int.Parse(Console.ReadLine());
+// if (N>999) Console.WriteLine("invalid number");
+Console.WriteLine(N%8);
