89facf4f6d9086a22d336de6410574c4a5052373
TheDemiurgePepe <orlovskiy-maksimka@mail.ru>
2023-04-21 01:26:00 +0500

    end home work

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -19,7 +19,7 @@
 //     Console.Write($"{a} not quad {b}");
 // }
 
-// Task3. Напишите программу которая принимает на вход число N а на выходе мы получаем строчку от -N до N
+// // Task3. Напишите программу которая принимает на вход число N а на выходе мы получаем строчку от -N до N
 // Console.Write("input number N:");
 // int N=int.Parse(Console.ReadLine());
 // if (N<=0) Console.WriteLine("not correct number");
@@ -30,9 +30,9 @@
 //     count++;
 // }
 
-// task3.  на вход принимаем 3х значное число
-// вывести последнюю цифру числа
-Console.Write("input three-digit number N:");
-int N=int.Parse(Console.ReadLine());
-// if (N>999) Console.WriteLine("invalid number");
-Console.WriteLine(N%8);
+// // task3.  на вход принимаем 3х значное число
+// // вывести последнюю цифру числа
+// Console.Write("input three-digit number N:");
+// int N=int.Parse(Console.ReadLine());
+// // if (N>999) Console.WriteLine("invalid number");
+// Console.WriteLine(N%8);
--- homeWork/Task.cs
+++ homeWork/Task.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write("input number N:");
+// int N=int.Parse(Console.ReadLine());
+// Console.Write("input number B:");
+// int B=int.Parse(Console.ReadLine());
+// if (N==B) 
+// {
+//     Console.WriteLine("invalid number");
+// }
+// if (N<B) 
+// {
+//     Console.WriteLine($"B= {B}> N={N}");
+// }
+// else  Console.WriteLine($"N= {N}> B={B}");
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.WriteLine("Введите три числа чтобы узнать большее из них: ");
+// Console.Write("Введите A= ");
+// double A = double.Parse(Console.ReadLine());
+// Console.Write("Введите B= ");
+// double B = double.Parse(Console.ReadLine());
+// Console.Write("Введите C= ");
+// double C = double.Parse(Console.ReadLine());
+// double max = A;
+// if (A > max) ;
+// {
+//     max = A;
+// }
+// if (B > max)
+// {
+//     max = B;
+// }
+
+// if (C > max)
+// {
+//     max = C;
+// }
+// Console.WriteLine($"Самое большое число {max}");
+   
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.WriteLine("Введите число и узнайте чётное ли оно: ");
+// double a = double.Parse(Console.ReadLine());
+// if(a%2==0)
+// {
+//     Console.WriteLine($"Число {a} чётное");
+// }
+// else Console.WriteLine($"Число {a} нечётное");
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Console.Write("input number N:");
+// int N=int.Parse(Console.ReadLine());
+// int count=2;
+// while(count%2==0 && count<=N)
+// {
+//     Console.Write(count+" ");
+//     count= count+2;
+// }
