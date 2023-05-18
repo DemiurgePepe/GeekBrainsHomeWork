c1f9700c877362c7bfcd620286093c83e36a23a3
TheDemiurgePepe <orlovskiy-maksimka@mail.ru>
2023-05-18 21:56:24 +0500

    Закончилась 4 лекция тема методы

--- hobby/Work.cs
+++ hobby/Work.cs
@@ -1,7 +1,12 @@
 ﻿// Для работы
-int count=1;
-while(count<2600)
-{
-Directory.CreateDirectory($"{count}");
-count= count + 14;
-}
+// int count=1;
+// while(count<2900)
+// {
+// Directory.CreateDirectory($"{count}");
+// count= count + 12;
+// }
+// for(int count=1;count<2900;count= count + 14)
+// {
+// Directory.CreateDirectory($"{count}");
+// count= count + 12;
+// }
