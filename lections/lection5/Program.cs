2544464e32c7e854f27eac623991332d3d37ea23
TheDemiurgePepe <orlovskiy-maksimka@mail.ru>
2023-05-23 23:11:16 +0500

    Закончил 5 лекцию

---
+++ lections/lection5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// void DrawText(string text, int left, int top)
+// {
+//     Console.SetCursorPosition(left,top);
+//     Console.WriteLine(text);
+// }
+// DrawText("Уга буга",629,360);
+
+// string text = "(1,2)"
+
+
+
+
