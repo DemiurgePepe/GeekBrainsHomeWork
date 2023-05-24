d79401df7a52bde2fb487de072482c44c500df57
TheDemiurgePepe <orlovskiy-maksimka@mail.ru>
2023-05-25 00:31:55 +0500

    Закончил 6 лекцию

---
+++ lections/lection 6/Program.cs
@@ -0,0 +1,23 @@
+﻿void Towers(string  with = "1", string on = "3", string some = "2", int count = 3)
+{
+
+    if( count>1) Towers(with,some,on, count -1);
+    Console.WriteLine($"{with}>>{on}");
+    if(count>1) Towers(some,on,with, count-1);
+    Console.WriteLine();
+}
+Towers();
+
+// DataTime dt = DateTime.Now;
+// decimal fRec = 0;
+// decimal FibonaccRecursion(int n)
+// {
+//     fRec++;
+//     return n==0 || n==1 ? 1 :FibonaccRecursion(n-1)+FibonaccRecursion(n-2);
+// }
+// for (int n = 10; n < 40; n++)
+// {
+//     Console.WriteLine($"FibonaccRecursion({n})= {FibonaccRecursion(n)} fRec={fRec}");
+//     fRec=0;
+// }
+// System.Console.WriteLine((DateTime.Now-dt).TotalMilliseconds);
