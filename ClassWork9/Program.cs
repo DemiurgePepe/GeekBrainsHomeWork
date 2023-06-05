22458f1329a14c24c38a4045bf1ccc706ff64720
TheDemiurgePepe <orlovskiy-maksimka@mail.ru>
2023-06-05 23:49:23 +0500

    начал делать 9дз

--- ClassWork9/Program.cs
+++ ClassWork9/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int UserInput(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+//Задача 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+// void SumNumbers(int n)
+// {
+//     if(n>0) 
+//     {
+//         SumNumbers(n-1);
+//         Console.Write(n+" ");
+//     }
+// }
+// int N = 5;
+// SumNumbers(5);
+
+
+//Задание 2. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+//453 -> 12
+//45 -> 9
+
+
+// int FindSum(int number)
+// {
+//     if(number>0)
+//     {
+//         return FindSum(number/10)+number % 10;
+//     }
+//     else return number;
+// }
+// int sum=FindSum(6392);
+// System.Console.WriteLine(sum);
+
+
+//Задача 3. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 5, 6, 7, 8"
+// void FindNumber(int m, int n)
+// {
+//     if(m<n) 
+//     {
+//         Console.Write(m+" ");
+//         FindNumber(m+1,n);
+//     }
+//     else if (m>n)
+//     {
+//         System.Console.Write(m+" ");
+//         FindNumber(m-1,n);
+//     }
+//     else System.Console.WriteLine(m);
+// }
+// int m = UserInput("Введите m: ");
+// int n = UserInput("Введите n: ");
+// FindNumber(m,n);
+
+//Задача 4.Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+// int AStapeB(int a, int b)
+// {
+//     if(b>0)
+//     {
+//         return AStapeB(a,b-1)*a;
+//     }
+//     else return 1;
+// }
+
+// int a = UserInput("Введите a: ");
+// int b = UserInput("Введите b: ");
+// int number = AStapeB(a,b);
+// System.Console.WriteLine(number);
+
---
+++ homeWork9/Program.cs
@@ -0,0 +1,52 @@
+﻿
+int UserInput(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+// void SumNumbers(int n)
+// {
+//     if(n>0) 
+//     {
+//         Console.Write(n+" ");
+//         SumNumbers(n-1);
+//     }
+// }
+// int N = 5;
+// SumNumbers(5);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void FindNumber(int m, int n)
+{
+    if(m<n) 
+    {
+        Console.Write(m+" ");
+        FindNumber(m+1,n)+m;
+    }
+    else if (m>n)
+    {
+        System.Console.Write(m+" ");
+        FindNumber(m-1,n);
+    }
+    else System.Console.WriteLine(m);
+}
+int m = UserInput("Введите m: ");
+int n = UserInput("Введите n: ");
+FindNumber(m,n);
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
